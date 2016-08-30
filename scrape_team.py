6094b7a7434b847e112c2eebad51e64bc94bdd5b
Bemmu Sepponen <manga@bemmu.com>
2016-08-30 15:43:52 +0900

    Clean up code to use only requests, move imports to top.

--- scrape_team.py
+++ scrape_team.py
@@ -1,18 +1,26 @@
-import urllib
-# url = "http://www.buzzcapture.com/en/about-buzzcapture/team/"
-# html_doc = urllib.urlopen(url, "r").read()
-html_doc = open("team.html", "r").read()
-
-# print html_doc
+# Scrapes BuzzCapture team page and uploads to ElasticSearch.
+# http://awstutorialelastic-1527363486.us-east-1.elb.amazonaws.com:9200/_plugin/head/
 
 from BeautifulSoup import BeautifulSoup
-soup = BeautifulSoup(html_doc)
+import json
+import requests
+
+team_url = "http://www.buzzcapture.com/en/about-buzzcapture/team/"
+elasticsearch_endpoint = "http://awstutorialelastic-1527363486.us-east-1.elb.amazonaws.com:9200/team/profile"
+
+soup = BeautifulSoup(requests.get(team_url).text)
 people = soup.findAll('div', {'class': 'col description'})
+
 for person in people:
 	desc = person.find('blockquote').text
 	name, role = person.find('p').text.split(",")
-	print desc
 
-#print people
+	data = json.dumps({
+	    "name" : name,
+	    "role" : role,
+	    "desc" : desc
+	})
 
-#print(soup.prettify())
+	print "Would insert: %s" % data
+#	r = requests.post(elasticsearch_endpoint, data=data)
+#	print r.content
